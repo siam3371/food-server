6d2b98aae0f9c3075a1510ee303d30ce7ea48e25
siam3371 <siamhossain3371@gmail.com>
2021-10-31 21:37:19 +0600

    first

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+var ObjectId = require('mongodb').ObjectID;
+
 const port =process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config() 
@@ -8,24 +10,29 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
  const MongoClient = require('mongodb').MongoClient; 
- 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c6mfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri)
-
+ 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
     try {
       await client.connect();
-      console.log('client connection successful' )
-      const food = client.db("food_server");
+       const food = client.db("food_server");
       const product = food.collection("product");
+//  GET API
        app.get('/product', async(req, res)=> {
         const cursor = product.find({});
      const products = await cursor.toArray();
              res.send(products) 
        }) 
-    
+//  POST API
+app.post( '/product',async(req, res) => {
+  const newService = req.body;
+  const result = await product.insertOne(newService)    
+console.log('hitting the poast',req.body)
+console.log('add a user', result)
+res.json(result)
+})
      } 
      finally {
     //   await client.close();
